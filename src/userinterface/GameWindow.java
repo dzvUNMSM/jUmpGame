6646d2efa7d9a564e264aab5af246905e7e9fa94
dzvCode <diego.zavala@unmsm.edu.pe>
2022-01-19 17:21:47 +0000

    Cambio de nombre de ventana

--- src/userinterface/GameWindow.java
+++ src/userinterface/GameWindow.java
@@ -8,7 +8,7 @@ public class GameWindow extends JFrame {
 	private GameScreen gameScreen;
 	
 	public GameWindow() {
-		super("j (U) mp Game");
+		super("probando cambios");
 		setSize(SCREEN_WIDTH, 175);
 		setLocation(400, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
